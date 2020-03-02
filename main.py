596b10c5fe56e3130b69609e8a6e32ba41c5c08f
Chris Lopez <pagarizabal2@gmail.com>
2020-03-02 23:42:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+
+web = requests.get('https://www.worldometers.info/coronavirus/#countries')
+
+soup = BeautifulSoup(web.text, features="html.parser")
+
+_id = "main_table_countries"
+
+raw_data = soup.find("table", attrs={"id": _id}).find("tbody").findAll("tr")
+
+parsed_data = []
+
+for country in raw_data:
+  parsed_data.append([data.get_text() for data in country.findAll("td")])
+
+COLUMNS = ["Country", "Total Cases", "New Cases", "Total Deaths", "New Deaths", "Active Cases", "Recovered", "Critical"]
+
+#pd.set_option('display.max_rows', None)
+#pd.set_option('display.max_columns', None)
+
+final = pd.DataFrame(parsed_data, columns=COLUMNS)
+
+#final.to_csv(r'./cases.csv', index = False)
+print(final.to_string)
